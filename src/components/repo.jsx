d58459905da95c7d33dfe6dd56ccf49da8bd7ee5
deji100 <adeyemideji9@gmail.com>
2022-05-26 22:35:01 +0100

    Initial commit

---
+++ src/components/repo.jsx
@@ -0,0 +1,23 @@
+const Repository = (props) => {
+    const { owner, stargazers_count, open_issues, name, description, created_at } = props.item
+    return (
+        <div key={owner.id} className='content'>
+            <div className="avatar">
+                <img src={owner.avatar_url} alt="" />
+            </div>
+            <div className="detail">
+                <div className="name-description">
+                    <h1>{name}</h1>
+                    <p>{description}</p>
+                </div>
+                <div className="star-issues-time">
+                    <p className="star">Stars: {stargazers_count}</p>
+                    <p className="issue">Issues: {open_issues}</p>
+                    <p>Submitted {Math.floor((new Date().getTime() - new Date(created_at).getTime()) / (1000 * 60 * 60 * 24))} days by {owner.login}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Repository;
---
+++ src/components/repos.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react"
+import Repository from "./repo"
+
+const Repositories = () => {
+    const [repos, setRepos] = useState([])
+
+    useEffect(() => {
+        const url = 'https://api.github.com/search/repositories?q=created:>2021-08-13&sort=stars&order=desc'
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setRepos(data.items))
+    }, [])
+
+    console.log(repos)
+    return (
+        <div>
+            {repos.map(item => {
+                return <Repository key={item.owner.id} item={item} />
+            })}
+        </div>
+    )
+}
+
+export default Repositories;
